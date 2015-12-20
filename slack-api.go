eb4035310c2f0089520b5a6e24e2b9ad06495a18
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2015-12-20 16:53:01 +0200

    Vendoring dependencies

--- slack-api.go
+++ slack-api.go
@@ -3,7 +3,7 @@ package wb
 import (
 	"encoding/json"
 	"fmt"
-	"golang.org/x/net/websocket"
+	"github.com/sger/weather-bot/Godeps/_workspace/src/golang.org/x/net/websocket"
 	"io/ioutil"
 	"log"
 	"net/http"
